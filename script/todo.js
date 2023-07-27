28444e9d27d2b073ae1e442f1f5e5b9e2ccd5c8f
Danish Esmolada <99858889+nishesmolada@users.noreply.github.com>
2023-07-27 14:57:55 +0800

    Update todo.js

--- script/todo.js
+++ script/todo.js
@@ -8,22 +8,22 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = "";
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    const name = todoObject.name;
-    const dueDate = todoObject.dueDate;
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate} = todoObject;
 
     const html = `
     <p>
       ${name} ${dueDate} 
-      <button class="js-delete-todo" onclick='todoList.splice(${i}, 1); 
+      <button class="js-delete-todo" onclick='todoList.splice(${index}, 1); 
       renderTodoList();'
       >Delete</button>
     </p>`;
     todoListHTML += html;
-  }
+
+  });
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
+
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
